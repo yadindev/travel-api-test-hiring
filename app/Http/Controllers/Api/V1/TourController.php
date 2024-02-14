1ad8d387063c007249a0445baae5e56b2376d013
Yadin Dominguez <yadinds123@gmail.com>
2024-02-13 23:51:38 -0500

    Add tour endpoints and test

--- app/Http/Controllers/Api/V1/TourController.php
+++ app/Http/Controllers/Api/V1/TourController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\TourListRequest;
 use App\Http\Resources\TourResource;
 use App\Models\Tour;
 use App\Models\Travel;
@@ -11,9 +12,24 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class TourController extends Controller
 {
-    public function index(Travel $travel): JsonResource
+    public function index(Travel $travel, TourListRequest $request): JsonResource
     {
         $tours = $travel->tours()
+            ->when($request->priceFrom, function ($query) use ($request) {
+                $query->where('price', '>=', $request->priceFrom);
+            })
+            ->when($request->priceTo, function ($query) use ($request) {
+                $query->where('price', '<=', $request->priceTo);
+            })
+            ->when($request->dateFrom, function ($query) use ($request) {
+                $query->where('starting_date', '>=', $request->dateFrom);
+            })
+            ->when($request->dateTo, function ($query) use ($request) {
+                $query->where('starting_date', '<=', $request->dateTo);
+            })
+            ->when($request->sortBy && $request->sortOrder, function ($query) use ($request) {
+                $query->orderBy($request->sortBy, $request->sortOrder);
+            })
             ->orderBy('starting_date')
             ->paginate();
         return TourResource::collection($tours);
---
+++ tests/Feature/TourListTest.php
@@ -0,0 +1,60 @@
+<?php
+
+use App\Models\Tour;
+use App\Models\Travel;
+
+test('tour list by travel slug return correct tours', function () {
+    $travel = Travel::factory()->create();
+    $tour = Tour::factory()->create(['travel_id' => $travel->id]);
+
+    $response = $this->get('/api/v1/travels/' . $travel->slug . '/tours');
+
+    $response->assertStatus(200);
+    $response->assertJsonCount(1, 'data');
+    $response->assertJsonFragment(['id' => $tour->id]);
+});
+
+test('tour price is shown correctly', function () {
+    $travel = Travel::factory()->create();
+    Tour::factory()->create([
+        'travel_id' => $travel->id,
+        'price' => 123.45,
+    ]);
+
+    $response = $this->get('/api/v1/travels/' . $travel->slug . '/tours');
+
+    $response->assertStatus(200);
+    $response->assertJsonCount(1, 'data');
+    $response->assertJsonFragment(['price' => '123.45']);
+});
+
+test('tour list returns pagination', function () {
+    $travel = Travel::factory()->create();
+    Tour::factory(16)->create(['travel_id' => $travel->id]);
+
+    $response = $this->get('/api/v1/travels/' . $travel->slug . '/tours');
+
+    $response->assertStatus(200);
+    $response->assertJsonCount(15, 'data');
+    $response->assertJsonPath('meta.last_page', 2);
+});
+
+test('tours list sorts by starting date correctly', function () {
+    $travel = Travel::factory()->create();
+    $laterTour = Tour::factory()->create([
+        'travel_id' => $travel->id,
+        'starting_date' => now()->addDays(2),
+        'ending_date' => now()->addDays(3),
+    ]);
+    $earlierTour = Tour::factory()->create([
+        'travel_id' => $travel->id,
+        'starting_date' => now(),
+        'ending_date' => now()->addDays(1),
+    ]);
+
+    $response = $this->get('/api/v1/travels/' . $travel->slug . '/tours');
+
+    $response->assertStatus(200);
+    $response->assertJsonPath('data.0.id', $earlierTour->id);
+    $response->assertJsonPath('data.1.id', $laterTour->id);
+});
