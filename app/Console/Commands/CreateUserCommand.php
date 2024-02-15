01c2af4382cdd2d12e1ab0a58cf5986c5a550a19
Yadin Dominguez <yadinds123@gmail.com>
2024-02-14 23:38:47 -0500

    Add users:create command

---
+++ app/Console/Commands/CreateUserCommand.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rules\Password;
+
+class CreateUserCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'users:create';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command to create a new user';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $user['name'] = $this->ask('Name of the new user');
+        $user['email'] = $this->ask('Email of the new user');
+        $user['password'] = $this->secret('Password of the new user');
+        $roleName = $this->choice('Rol of user', ['admin', 'editor'], 'editor');
+        $role = Role::where('name', $roleName)->first();
+        if (!$role) {
+            $this->error('Role not found');
+            return -1;
+        }
+
+        $validator = Validator::make($user, [
+            'name' => 'required|string|max:255',
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:' . User::class],
+            'password' => ['required', Password::defaults()]
+        ]);
+
+        if ($validator->fails()) {
+            foreach ($validator->errors()->all() as $error) {
+                $this->error($error);
+            }
+            return -1;
+        }
+
+        $user['password'] = Hash::make($user['password']);
+
+        DB::transaction(function () use ($user, $role) {
+            $newUser = User::create($user);
+            $newUser->roles()->attach($role->id);
+        });
+
+        $this->info('New user ' . $user['email'] . ' created succefully');
+
+        return 0;
+    }
+}
